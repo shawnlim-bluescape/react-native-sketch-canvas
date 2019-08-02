c77cd02e3752c39e46cc650caffd9d1e1f8f2271
Shawn Lim <shawn.lim@bluescape.com>
2019-08-02 16:04:30 -0700

    Set permissions to true. No external write required.

--- src/handlePermissions.js
+++ src/handlePermissions.js
@@ -1,20 +1,5 @@
 import { PermissionsAndroid, Platform } from 'react-native';
 
 export const requestPermissions = async (permissionDialogTitle, permissionDialogMessage) => {
-  return false;
-  /*
-    if (Platform.OS === 'android') {
-        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
-            title: permissionDialogTitle,
-            message: permissionDialogMessage,
-        });
-
-        // On devices before SDK version 23, the permissions are automatically granted if they appear in the manifest,
-        // so check and request should always be true.
-        // https://github.com/facebook/react-native-website/blob/master/docs/permissionsandroid.md
-        const isAuthorized = Platform.Version >= 23 ? granted === PermissionsAndroid.RESULTS.GRANTED : granted === true;
-        return isAuthorized;
-    }
-    return true;
-    */
+  return true;
 }
